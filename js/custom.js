0457fc6e14e4507f7949be965ddd89ba8196b185
Terence Foxcroft <tfoxcroft@gmail.com>
2019-11-29 19:08:14 +0200

    Update

--- js/custom.js
+++ js/custom.js
@@ -56,6 +56,20 @@
 		errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
 	});
 
+	$('.owl-single .item a').nivoLightbox({
+		effect: 'fadeScale',                             // The effect to use when showing the lightbox
+		theme: 'default',                           // The lightbox theme to use
+		keyboardNav: true,                          // Enable/Disable keyboard navigation (left/right/escape)
+		clickOverlayToClose: true,                  // If false clicking the "close" button will be the only way to close the lightbox
+		onInit: function(){},                       // Callback when lightbox has loaded
+		beforeShowLightbox: function(){},           // Callback before the lightbox is shown
+		afterShowLightbox: function(lightbox){},    // Callback after the lightbox is shown
+		beforeHideLightbox: function(){},           // Callback before the lightbox is hidden
+		afterHideLightbox: function(){},            // Callback after the lightbox is hidden
+		onPrev: function(element){},                // Callback when the lightbox gallery goes to previous item
+		onNext: function(element){},                // Callback when the lightbox gallery goes to next item
+		errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
+	});
 
 	//parallax
         if ($('.parallax').length)
@@ -93,6 +107,12 @@
 			$('#galleryLink').click(function (){
 				$('html, body').animate({
 				    scrollTop: ($('#gallery').offset().top)
+				},500);
+			});
+
+			$('#thankyouLink').click(function (){
+				$('html, body').animate({
+				    scrollTop: ($('#thankyou').offset().top)
 				},500);
 			});
 
